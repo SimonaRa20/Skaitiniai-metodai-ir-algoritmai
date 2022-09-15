75b520c40e8d85313010b7935d9b420a13c12601
SimonaRa20 <simonaaragauskaite@gmail.com>
2022-09-15 17:17:14 +0300

    made minimal changes

--- laboratorinis1/tekstinis.py
+++ laboratorinis1/tekstinis.py
@@ -3,27 +3,31 @@ from scipy.optimize import fsolve
 import scanDecreasing as scan
 import matplotlib.pyplot as plt
 import sys
+import scipy
 
 def getMass(m):
     return 80 * np.exp(-(0.1 * 4)/m) + m * 9.8 / 0.1 * (np.exp(-(0.1 * 4)/m) - 1) - 21
 
 def scanDecreasing(x1, x2):
     zingsnis = (x2 - x1) / 100
-    print(zingsnis)
     while np.abs(getMass(x1)) > 0.001 and x1 + zingsnis <= x2:
         if np.sign(getMass(x1)) != np.sign(getMass(x1 + zingsnis)):
             zingsnis /= 2
             continue
         x1 += zingsnis
-        print(zingsnis)
     return x1
 
 ax = plt.subplot()
 start = 0.5
 end = 1.5
 x = np.arange(start, end, 0.01)
+
+f = lambda m: 80 * np.exp(-(0.1 * 4)/m) + m * 9.8 / 0.1 * (np.exp(-(0.1 * 4)/m) - 1) - 21
+
 afterFunctionX = scanDecreasing(start,end)
-print(afterFunctionX)
+root = scipy.optimize.brentq(f, start, end)
+print('saknis - ',afterFunctionX)
+print('tikslumas - ', root - afterFunctionX)
 ax.grid()
 ax.plot(x, getMass(x))
 plt.show()
